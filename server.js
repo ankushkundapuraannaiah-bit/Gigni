9b3430c161034f9853622117ae9957bcd8cbd985
ankushkundapuraannaiah-bit <ankushkundapuraannaiah@gmail.com>
2026-05-09 13:06:14 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const express = require('express');
 const app = require('./api/index');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
     console.log(`Node server running locally on http://localhost:${PORT}`);
